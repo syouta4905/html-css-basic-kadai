275499980df1aef4ee758ec6414988eb725204a9
Inoue Syouta <s.lvxxx0301@ezweb.ne.jp>
2024-08-23 12:28:48 +0900

    Add files via upload

---
+++ kadai_014/square.js
@@ -0,0 +1,7 @@
+// 値を二乗する関数を作成し、定数に代入
+const square = function(number) {
+   return number * number;
+}
+// 関数を呼び出して、結果をコンソールに出力
+const result = square(10);
+console.log(result); // コンソールに 100 が表示されます
