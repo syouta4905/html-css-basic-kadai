0e0827cd69308a24520d4fe221a3e160386a24b3
Inoue Syouta <s.lvxxx0301@ezweb.ne.jp>
2024-09-03 12:15:15 +0900

    Delete kadai_005 directory

--- kadai_005/bmi.js
+++
@@ -1,9 +0,0 @@
-// 体重と身長を変数に代入
-const weight = 68; // 体重 (kg)
-const height = 1.7; // 身長 (m)
-// BMIを計算
-const bmi = weight / (height * height);
-// 結果をコンソールに出力
-console.log(`体重: ${weight}kg`);
-console.log(`身長: ${height}m`);
-console.log(`BMI: ${bmi.toFixed(2)}`);
