5ebe971783ca3650ce954c7d83b966a68afac02f
Inoue Syouta <s.lvxxx0301@ezweb.ne.jp>
2024-09-03 12:14:26 +0900

    Delete kadai_014 directory

--- kadai_014/square.js
+++
@@ -1,7 +0,0 @@
-// 値を二乗する関数を作成し、定数に代入
-const square = function(number) {
-   return number * number;
-}
-// 関数を呼び出して、結果をコンソールに出力
-const result = square(10);
-console.log(result); // コンソールに 100 が表示されます
