e11adb9349570fd590cab289c1757b3174df884e
Inoue Syouta <s.lvxxx0301@ezweb.ne.jp>
2024-12-10 12:43:34 +0900

    Delete kadai_020 directory

--- kadai_020/event.js
+++
@@ -1,8 +0,0 @@
-// HTML要素を取得して定数に代入
-const button = document.getElementById('btn');
-const textElement = document.getElementById('text');
-
-// ボタンがクリックされたときに実行される関数を定義
-button.addEventListener('click', () => {
- textElement.textContent = 'ボタンがクリックされました！';
-});
