0ffd4ad866b70bfe078df4346550e7b653545d04
Inoue Syouta <s.lvxxx0301@ezweb.ne.jp>
2024-12-24 15:19:44 +0900

    Add files via upload

---
+++ kadai_app_web/complete.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>完了フォーム</title>
+</head>
+<body>
+<h1>登録が完了しました。</h1>
+<h2>社員情報がデータベースに保存されました。</h2>
+<form action="form.php" method="get">
+<button type="submit">戻る</button>
+</form>
+</body>
+</html>
